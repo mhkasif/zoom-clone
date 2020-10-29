dacbf5c95d411107ff5e6715ac6c11537d415983
mhkasif <haseebasif97@gmail.com>
2020-10-30 00:13:23 +0500

    dep

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
   });
 });
 if (process.env.NODE_ENV === "production")
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 //   app.use(express.static("../client/build"));
 
 const port = process.env.PORT || 5000;
